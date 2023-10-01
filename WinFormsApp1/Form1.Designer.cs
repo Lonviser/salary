92755083b2f49e6af850e18acf0cfa8286a3afe3
Ivan Lenkavets <47rex47@mail.ru>
2023-10-01 20:37:50 +0300

    Логика рассчёта

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -31,6 +31,30 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -46,7 +70,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(48, 82);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.Size = new System.Drawing.Size(214, 27);
             this.textBox1.TabIndex = 1;
             // 
             // label2
@@ -54,15 +78,238 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(48, 131);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.Size = new System.Drawing.Size(177, 20);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "Количество детей до 18:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(48, 169);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(214, 27);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(214, 40);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Количество детей страше 18, \r\nполучающих образование:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(48, 284);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(214, 27);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 151);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Расчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(589, 467);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(214, 27);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(589, 433);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "К выдаче:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(320, 82);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(214, 27);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.Text = "13";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(320, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Подоходный налог:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(320, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Пенсионный взнос:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(320, 169);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(214, 27);
+            this.textBox6.TabIndex = 12;
+            this.textBox6.Text = "1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(320, 231);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(159, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Профсоюзный взнос:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(320, 284);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(214, 27);
+            this.textBox7.TabIndex = 14;
+            this.textBox7.Text = "1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(589, 48);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(196, 20);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Льгота на одного ребёнка:";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(589, 82);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(227, 27);
+            this.textBox8.TabIndex = 16;
+            this.textBox8.Text = "46";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(589, 131);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(227, 20);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Льгота на одного ребёнка от 2:";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(589, 169);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(227, 27);
+            this.textBox9.TabIndex = 18;
+            this.textBox9.Text = "87";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(589, 231);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(276, 40);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Налоговый вычет за детей страше 18, \r\nполучающих образование:";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(589, 284);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(214, 27);
+            this.textBox10.TabIndex = 20;
+            this.textBox10.Text = "1";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(589, 349);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(146, 20);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Подоходный налог:";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(320, 467);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(214, 27);
+            this.textBox11.TabIndex = 22;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(320, 432);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(87, 20);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "Отчислено:";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(589, 384);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(214, 27);
+            this.textBox12.TabIndex = 24;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(320, 384);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(214, 27);
+            this.textBox13.TabIndex = 26;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(320, 349);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(230, 20);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "Стандартный налоговый вычет:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(897, 548);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -78,5 +325,29 @@
         private Label label1;
         private TextBox textBox1;
         private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Button button1;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox5;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox6;
+        private Label label7;
+        private TextBox textBox7;
+        private Label label8;
+        private TextBox textBox8;
+        private Label label9;
+        private TextBox textBox9;
+        private Label label10;
+        private TextBox textBox10;
+        private Label label11;
+        private TextBox textBox11;
+        private Label label12;
+        private TextBox textBox12;
+        private TextBox textBox13;
+        private Label label13;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,10 +1,92 @@
+using System;
+using System.Windows.Forms;
+using System.Reflection;
+
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        private SalaryCalculator salaryCalculator;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double salary = double.Parse(textBox1.Text);
+            double numberOfChildren = double.Parse(textBox2.Text);
+            double standardTaxDeduction = double.Parse(textBox3.Text);
+
+            double[] calculatedSalary = { salary, numberOfChildren, 10000, 0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; 
+            SalaryCalculator calculator = new SalaryCalculator(calculatedSalary);
+            MessageBox.Show($"Размер начисленной зарплаты: {calculator.CalculateSalary()}");
+        }
+
+        public class SalaryCalculator
+        {
+            private double salary; //зп
+            private double accrued; //начислено
+            private double numberOfChildren; //Количество детей 
+            private double standardTaxDeduction; // стандартный налоговый вычет (до какой суммы)
+            private double standardTaxDeductionCount;// стандартный налоговый вычет (его значение)
+            private double incomeTax; // Подоходный налог
+            private double incomeTaxPercent; // Подоходный налог
+            private double expelled; //отчислено
+            private double numberOfOldChildren; //Количество детей cтарше 18 получающих образование
+            private double taxDeduction; // налоговый вычет 
+            private double lnor1; // льгота на одного ребёнка
+            private double lnor2; // льгота на одного ребёнка от 2 детей
+            private double creditPayment; // Кредитный платёж
+            private double tuitionAmount; //сумма обучения
+            private double pensionFund;// Пенсионный фонд
+            private double unionDues; //профсоюзные взносы
+
+            public SalaryCalculator(double[] initialValues)
+            {
+                if (initialValues.Length != 15) 
+                {
+                    throw new ArgumentException("Array length should be 15.", nameof(initialValues));
+                }
+
+                salary = initialValues[0];
+                numberOfChildren = initialValues[1];
+                standardTaxDeduction = initialValues[2];
+                standardTaxDeductionCount = initialValues[3];
+                incomeTax = initialValues[4];
+                incomeTaxPercent = initialValues[5];
+                expelled = initialValues[6];
+                numberOfOldChildren = initialValues[7];
+                taxDeduction = initialValues[8];
+                lnor1 = initialValues[9];
+                lnor2 = initialValues[10];
+                creditPayment = initialValues[11];
+                tuitionAmount = initialValues[12];
+                pensionFund = initialValues[13];
+                unionDues = initialValues[14];
+            }
+
+            public double CalculateSalary()
+            {
+                // Вычисления зарплаты и налогов
+                if (salary < standardTaxDeduction)
+                {
+                    double lnor = (numberOfChildren > 1) ? lnor2 : lnor1;
+                    incomeTax = (salary - standardTaxDeduction - taxDeduction * numberOfOldChildren - lnor * numberOfChildren - creditPayment - tuitionAmount) * incomeTaxPercent / 100;
+                }
+                else
+                {
+                    double lnor = (numberOfChildren > 1) ? lnor2 : lnor1;
+                    incomeTax = (salary - taxDeduction * numberOfOldChildren - lnor * numberOfChildren - creditPayment - tuitionAmount) * incomeTaxPercent / 100;
+                }
+
+                expelled = incomeTax + salary * pensionFund + salary * unionDues;
+                accrued = salary - expelled;
+
+                return accrued;
+            }
         }
     }
 }
